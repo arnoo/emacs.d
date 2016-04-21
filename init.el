d970a08e72ada652213b337590640f477f51d7a8
Arnaud Betremieux <arb@octo.com>
2016-04-21 09:44:20 +0200

    incron autocommit

--- init.el
+++ init.el
@@ -80,7 +80,7 @@
 
 ;;;; my-Packages
 (setq my-packages
-      '(package cl-lib color color-theme evil evil-numbers evil-tabs scala-mode2 js2-mode fiplr markdown-mode))
+      '(package cl-lib color color-theme evil evil-numbers evil-tabs scala-mode2 js2-mode fiplr markdown-mode evil-search-highlight-persist evil-quickscope))
 
 ; Install my-packages as necessary
 (defun filter (condp lst)
