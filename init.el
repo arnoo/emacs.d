4eab8e27abc61cb5cd98542d1e87caf4e8adede3
Arnaud Betremieux <arb@octo.com>
2016-04-21 09:45:11 +0200

    incron autocommit

--- init.el
+++ init.el
@@ -94,6 +94,7 @@
     (package-refresh-contents)
     (mapc 'package-install uninstalled-packages)))
 
+; This extension will make isearch and evil-ex-search-incremental to highlight the search term (taken as a regexp) in all the buffer and persistently until you make another search or clear the highlights with the evil-search-highlight-persist-remove-all command (default binding to C-x SPC). This is how Vim search works by default when you enable hlsearch.
 (require 'evil-search-highlight-persist)
 (global-evil-search-highlight-persist t)
 
