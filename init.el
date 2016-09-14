37cb89c47ef34a6a86823c75e334b4510d022961
Arnaud Betremieux <arb@octo.com>
2016-09-14 21:49:36 +0200

    incron autocommit

--- init.el
+++ init.el
@@ -228,10 +228,22 @@ otherwise, close current tab (elscreen)."
 
 ; *** JAVASCRIPT ***
 
+(defun ome-tern-setup ()
+  (when (el-get-package-installed-p 'js2-mode)
+    (add-hook 'js2-mode-hook (lambda () (tern-mode t))))
+  (when (el-get-package-installed-p 'js3-mode)
+    (add-hook 'js3-mode-hook (lambda () (tern-mode t))))
+  (setq tern-command (cons (executable-find "tern") '()))
+  (eval-after-load 'tern
+    '(progn
+      (require 'tern-auto-complete)
+      (tern-ac-setup))))
+
 (add-hook 'js-mode-hook
    (lambda () (push '("function" . ?ƒ) prettify-symbols-alist)
          (push '("return" . ?\u2192) prettify-symbols-alist)
-         (prettify-symbols-mode)))
+         (prettify-symbols-mode)
+         (ome-install 'tern)))
 
 ; *** LATEX ***
 
