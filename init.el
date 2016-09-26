76e654ea6ef3039f4b84137d8cabbe95c861d06e
Arnaud Betremieux <arb@octo.com>
2016-09-26 08:41:09 +0200

    incron autocommit

--- init.el
+++ init.el
@@ -232,7 +232,7 @@ otherwise, close current tab (elscreen)."
 (defun fd-switch-dictionary()
    (interactive)
    (let* ((dic ispell-current-dictionary)
- 	 (change (if (string= dic "francais") "english" "francais")))
+     (change (if (string= dic "francais") "english" "francais")))
      (ispell-change-dictionary change)
      (message "Dictionary switched from %s to %s" dic change)
      ))
