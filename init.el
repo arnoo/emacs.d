946fd1787229881e90d233e092319de19e34ea50
Arnaud Betremieux <arb@octo.com>
2016-10-06 09:54:15 +0200

    incron autocommit

--- init.el
+++ init.el
@@ -639,3 +639,17 @@ otherwise, close current tab (elscreen)."
 ;(setq evil-search-highlight-string-min-len 3)
 
 (server-start)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (neotree scala-mode2 markdown-mode js2-mode helm flycheck fiplr evil-tabs evil-search-highlight-persist evil-quickscope evil-numbers company-tern color-theme ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
