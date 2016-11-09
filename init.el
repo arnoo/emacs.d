f0431c5ad2c07e56419445d89e61370ae221baff
Arnaud Betremieux <arb@octo.com>
2016-11-09 10:25:08 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -290,7 +290,7 @@ otherwise, close current tab (elscreen)."
                (tern-ac-setup)))))
 
 (add-hook 'find-file-hook
-  (let ((bfn (buffer-file-name)))
+  (let ((bfn buffer-file-name))
     (when (and bfn (string-match "/node_modules/" bvn))
       (unless (y-or-n-p "WARNING: are you sure you want to edit a file from node_modules (n will set read-only mode) ?")
         (read-only-mode)))))
