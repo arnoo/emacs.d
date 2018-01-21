ecbe52e14bae6dee85bfe8a411ff9e88cebcaf14
Arnaud Betremieux <arb@octo.com>
2018-01-21 14:06:24 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -600,7 +600,9 @@ otherwise, close current tab (elscreen)."
   (mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder (mu4e-message-at-point)))))
 
 (defun mu4e-mute-thread ()
-  )
+  (interactive)
+  (let ((msg (mu4e-message-at-point)))
+    (mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder msg)))))
 
 (defun mu4e-msg-to-task ()
   "Archive a message and create a task in taskwarrior"
