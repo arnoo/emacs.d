cc7e881a04c9a895bf4d890ab5655e1ec17ae484
Arnaud Betremieux <arb@octo.com>
2018-01-22 20:38:26 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -611,8 +611,9 @@ otherwise, close current tab (elscreen)."
 (add-hook 'mu4e-index-updated-hook
   (defun my-mu4e-check-for-muted-threads ()
     ("maildir:/Octo_Inbox")
-    (plist-get msg :references)
-    (split-string orig-root " ")
+    
+    (replace-regexp-in-string "(<|>)" "" buffer-file-name))
+    (split-string orig-root (plist-get msg :references))
 
      )))
 
