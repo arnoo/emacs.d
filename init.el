d63ffe51497d8bcd0490e3a14a906cf195dff77f
Arnaud Betremieux <arb@octo.com>
2018-01-23 15:37:33 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -612,7 +612,7 @@ otherwise, close current tab (elscreen)."
 (defun my-mu4e-check-for-muted-threads ()
     (message "Checking for muted threads")
 ;    (setq mu4e-header-func 'my-mu4e-check-if-muted)
-      (mu4e~proc-find "maildir:/Octo_INBOX" t :date 'ascending nil nil nil)))
+      (mu4e~proc-find "maildir:/Octo_INBOX" t :date 'ascending nil nil nil))
 
 (add-hook 'after-init-hook 'my-mu4e-check-for-muted-threads)
 (add-hook 'mu4e-index-updated-hook 'my-mu4e-check-for-muted-threads)
