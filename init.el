7b32ad96d78c4d7e9f63c91fc6749d5ff32c760e
Arnaud Betremieux <arb@octo.com>
2018-01-23 08:51:59 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -646,6 +646,13 @@ otherwise, close current tab (elscreen)."
 
 (setq mu4e-headers-visible-lines 20)
 
+(setq mu4e-headers-fields
+      ((:human-date    .   12)
+      ;(:flags         .    6)
+       (:mailing-list  .   10)
+       (:from          .   22)
+       (:subject       .   nil)))
+
 (setq mu4e-headers-date-format "%Y-%m-%d %H:%M")
 (setq mu4e-headers-time-format "%H:%M")
 
