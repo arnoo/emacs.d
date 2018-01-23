d4e063015740ab604fde7d4137c291776a28d121
Arnaud Betremieux <arb@octo.com>
2018-01-23 15:19:48 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -628,7 +628,7 @@ otherwise, close current tab (elscreen)."
           (goto-char 1)
           (when (re-search-forward (concat "^" (car references) "$" nil t))
             ;(mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder (mu4e-message-at-point))))
-            (message "Muting thread")
+            (message "Thread is muted, auto-archiving")
             (setf found t))
           (setq references (cdr references))))))
 
