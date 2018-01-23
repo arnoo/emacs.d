8cf99b2653b8e85f032fcd1baa642d2507275e91
Arnaud Betremieux <arb@octo.com>
2018-01-23 08:31:06 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -608,23 +608,23 @@ otherwise, close current tab (elscreen)."
     (mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder msg)))))
 
 ;;TODO
-;(add-hook 'mu4e-index-updated-hook
-;  (defun my-mu4e-check-for-muted-threads ()
-;    (let ((mu4e-header-func 'my-mu4e-check-if-muted))
-;      (mu4e-proc-find "maildir:/Octo_Inbox" t nil nil nil))))
-;
-;(defun my-mu4e-check-if-muted (msg)
-;  (let ((found nil)
-;        (references (plist-get msg :references)))
-;     (with-temp-buffer
-;        (insert-file-contents "~/.muted-mailids" nil nil nil t)
-;        (while (and (not found)
-;                    (references))
-;          (goto-char 1)
-;          (when (search-forward (car references) nil t)
-;    (mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder (mu4e-message-at-point))))
-;            (setf found t))
-;          (setq references (cdr references)))))
+(add-hook 'mu4e-index-updated-hook
+  (defun my-mu4e-check-for-muted-threads ()
+    (let ((mu4e-header-func 'my-mu4e-check-if-muted))
+      (mu4e-proc-find "maildir:/Octo_Inbox" t nil nil nil))))
+
+(defun my-mu4e-check-if-muted (msg)
+  (let ((found nil)
+        (references (plist-get msg :references)))
+     (with-temp-buffer
+        (insert-file-contents "~/.muted-mailids" nil nil nil t)
+        (while (and (not found)
+                    (references))
+          (goto-char 1)
+          (when (re-search-forward (concat "^" (car references) "$" nil t)
+    (mu4e-headers-mark-thread-using-markpair '(refile . (mu4e-get-refile-folder (mu4e-message-at-point))))
+            (setf found t))
+          (setq references (cdr references)))))
 
 (defun my-mu4e-msg-to-task ()
   "Archive a message and create a task in taskwarrior"
