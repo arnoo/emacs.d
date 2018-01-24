e58c515073dbcd35fa92dbe183feae0986df122b
Arnaud Betremieux <arb@octo.com>
2018-01-24 14:26:33 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -614,6 +614,7 @@ otherwise, close current tab (elscreen)."
 (defun my-mu4e-mute-thread ()
   (interactive)
   (write-region (concat (plist-get (mu4e-message-at-point) :message-id) "\n") nil "~/.muted-mailids" 'append)
+  (sit-for 0.5)
   (my-mu4e-archive-thread))
 
 (defun my-mu4e-mute-thread-from-view (msg)
