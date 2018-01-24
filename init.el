1a7aff85818b90602237157c98bf95563239a2e5
Arnaud Betremieux <arb@octo.com>
2018-01-24 09:01:44 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -629,6 +629,7 @@ otherwise, close current tab (elscreen)."
     (mu4e~proc-find "maildir:/Octo_INBOX" t :date 'ascending nil nil nil))
 
 (defun my-mu4e~headers-header-handler ()
+  (mu4e~headers-header-handler)
   )
 
 (defun my-mu4e-check-if-muted (msg)
