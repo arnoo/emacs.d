3b556cfae6b9ecec496134ff083d8b66ded6dcc9
Arnaud Betremieux <arb@octo.com>
2018-02-20 16:54:23 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -657,7 +657,7 @@ otherwise, close current tab (elscreen)."
 (defun my-mu4e-headers-execute ()
   (interactive)
   (mu4e-mark-execute-all nil)
-  )
+  (mu4e-headers-rerun-search))
 
 (defun my-mu4e-view-msg-in-tab ()
   (interactive)
