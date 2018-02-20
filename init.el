d127752ff54ef5edddd72369b43642dcc2a2f38a
Arnaud Betremieux <arb@octo.com>
2018-02-20 16:49:22 +0100

    incron autocommit

--- init.el
+++ init.el
@@ -628,7 +628,7 @@ otherwise, close current tab (elscreen)."
 (define-key mu4e-view-mode-map (kbd "k") 'previous-line)
 (define-key mu4e-view-mode-map "\C-k" 'mu4e-view-headers-prev)
 (define-key mu4e-headers-mode-map (kbd "<f5>") 'mu4e-headers-rerun-search)
-(define-key mu4e-headers-mode-map (kbd "C-r") 'mu4e-headers-rerun-search)
+(define-key mu4e-headers-mode-map (kbd "\C-r") 'mu4e-headers-rerun-search)
 
 (define-prefix-command 'my-mu4e-g-map)
 (define-key mu4e-headers-mode-map "g" 'my-mu4e-g-map)
